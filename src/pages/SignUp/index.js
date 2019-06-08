7865afb9147be0942b776feb32fb3c928e99c4e6
Lucas Lacerda <lucaslacerdacl@gmail.com>
2019-06-08 15:10:29 -0300

    Create sign up page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react'
-import { Container } from './style';
+import PropTypes from 'prop-types';
+import { Container, Title, Email, EmailInput, EmailLabel, EmailTip, NextButton, NextButtonText, EmailInputWrapper } from './style';
 import LinearGradient from 'react-native-linear-gradient';
+import NavigationBar from '../../components/NavigationBar';
 
 export default class SignUp extends Component {
+
+  static propTypes = {
+    navigation: PropTypes.object
+  }
+
+  state = {
+    text: ''
+  }
+
+  isInputValid = () => {
+    const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return reg.test(this.state.text);
+  }
+
   render() {
     return (
-      <LinearGradient 
+      <LinearGradient
         style={Container.content}
         colors={['#4015e1', '#882ec4']}
         useAngle={true} angle={120} >
+        <NavigationBar
+          left
+          where="Login"
+          navigation={this.props.navigation} />
+          <Title>Create account</Title>
+          <Email>
+            <EmailLabel>What's your email?</EmailLabel>
+            <EmailInputWrapper>
+            <EmailInput 
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              onChangeText={(text) => this.setState({ text })}
+              value={this.state.text} />
+            </EmailInputWrapper>
+            <EmailTip>You'll have to confirm this e-mail later.</EmailTip>
+          </Email>
+          <NextButton style={{
+            opacity: this.isInputValid() ? 1 : 0.5
+          }}>
+            <NextButtonText>
+              NEXT
+            </NextButtonText>
+          </NextButton>
       </LinearGradient>
     )
   }
--- src/pages/SignUp/style.js
+++ src/pages/SignUp/style.js
@@ -1,5 +1,6 @@
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
 export const Container = StyleSheet.create({
   content: {
@@ -9,3 +10,67 @@ export const Container = StyleSheet.create({
     flexDirection: 'column'
   }
 });
+
+export const Title = styled.Text`
+  font-family: Montserrat;
+  font-weight: bold;
+  font-size: 25px;
+  color: #fff;
+  margin-bottom: 40px;
+`;
+
+
+export const Email = styled.View`
+  flex-direction: column;
+  align-items: flex-start;
+  width: 90%;
+`;
+
+export const EmailLabel = styled.Text`
+  font-family: Montserrat;
+  font-weight: bold;
+  font-size: 16px;
+  color: #fff;
+`;
+
+export const EmailInputWrapper = styled.View`
+  border-radius: 5px;
+  height: 50px;
+  width: 100%;
+  background-color: rgba(255, 255, 255, 0.5);
+  margin: 7px 0;
+`;
+
+export const EmailInput = styled.TextInput`
+  height: 50px;
+  width: 100%;
+  color: #fff;
+  padding: 0 10px;
+  font-family: Montserrat;
+  font-size: 15px;
+  font-weight: bold;
+`;
+
+export const EmailTip = styled.Text`
+  font-family: Montserrat;
+  font-size: 14px;
+  color: #fff;
+`;
+
+export const NextButton = styled.View`
+  margin-top: 40px;
+  width: 130px;
+  height: 45px;
+  border-radius: 30px;
+  background-color: #fff;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const NextButtonText = styled.Text`
+  font-family: Montserrat;
+  font-size: 14px;
+  font-weight: bold;
+  color: #000000;
+`;
